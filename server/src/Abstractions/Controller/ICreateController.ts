7ccdf08aefbb9ce829eba7b2966a0180ad33ea6a
Pedro Escumalha <pedrosimoes93@gmail.com>
2021-04-03 10:26:45 +0100

    refactor controllers abstactions

--- server/src/Abstractions/Controller/ICreateController.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-
-export interface ICreateController {
-    CreateRouter(): Router;
-}
--- server/src/Abstractions/Controller/IPostsController.ts
+++ server/src/Abstractions/Controller/IPostsController.ts
@@ -1,17 +1,9 @@
-import { IRouter } from "express";
-import { Post } from "src/Entities/Post";
-import { IDatabaseClientAsync } from "../IDatabaseClientAsync";
-import { ICreateController } from "./ICreateController";
+import { IController } from "./IController";
 import { IDelete } from "./IDelete";
 import { IGet } from "./IGet";
 import { IPost } from "./IPost";
 import { IPut } from "./IPut";
 
-export interface IPostsController extends ICreateController, IGet<IPostsController>, IPost<IPostsController>, IDelete<IPostsController>, IPut<IPostsController>
+export interface IPostsController extends IController, IGet<IPostsController>, IPost<IPostsController>, IDelete<IPostsController>, IPut<IPostsController>
 {
-    router: IRouter;
-    
-    basePath: string;
-
-    dbClient: IDatabaseClientAsync<Post>;
 }
